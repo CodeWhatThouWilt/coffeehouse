4768384936e2eca8f6d887392d6810b7ed517f19
Jacob North <yakenorth@protonmail.com>
2022-04-11 22:27:40 -0400

    added query, data normalization and return to get route for /servers

--- backend/routes/api/servers.js
+++ backend/routes/api/servers.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
+const { Server } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 
 
-router.get('/', asyncHandler((res,res) => {
+router.get('/', requireAuth, asyncHandler((res,res) => {
     const userId = req.user.id;
 
+    const servers = Server.findAll({
+        where: {
+            ownerId: userId
+        }
+    });
+
+    const normalizedServers = {};
+    servers.forEach(server => normalizedServers[server.id] = server);
+    return res.json(normalizedServers);
+    
 }));
 
 
