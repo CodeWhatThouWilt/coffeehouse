0d7da7cdbe9575d6d57d6a42a4b1d6d5afa48ec6
Jacob North <yakenorth@protonmail.com>
2022-04-16 05:06:53 -0400

    finished styling for create channel modal

--- frontend/src/components/CreateChannelForm/index.js
+++ frontend/src/components/CreateChannelForm/index.js
@@ -3,32 +3,57 @@ import { useState } from 'react';
 import { useParams, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { createChannel } from '../../store/servers';
+import unfilledRadio from '../../assets/unfilledRadio.svg';
+import filledRadio from '../../assets/filledInRadio.svg';
 
-const CreateChannelForm = () => {
+const CreateChannelForm = ({}) => {
     const dispatch = useDispatch();
     const { serverId } = useParams();
     const [name, setName] = useState('');
-    const [channelType, setChannelType] = useState('Text');
+    const [errors, setErrors] = useState([]);
 
     const submitHandler = () => {
         dispatch(createChannel({ name, serverId }))
+        .catch(res => {
+            const data = res.json();
+            data.errors && setErrors(data.errors);
+        });
     };
 
     return (
         <form onSubmit={() => submitHandler()}>
-            <div className='create-channel-form-container'>
-                <div>
+            <div className='create-channel-container'>
+                <div className='create-channel-top-text'>
                     <h1>
-                        Create {channelType} Channel
+                        Create Text Channel
                     </h1>
                 </div>
-                <form onSubmit={() => submitHandler()} id='new-channel-form'>
-                    <input
-                        value={name}
-                        onChange={e => setName(e.target.value.replace(' ', '-'))}
-                    />
-                </form>
-                <div onClick={() => submitHandler()} form='new-channel-form'>Submit</div>
+                <div className='create-channel-mid-section-container'>
+                    <div className='create-channel-channel-name-header'>
+                        CHANNEL NAME
+                    </div>
+                    <div className='create-channel-input-container'>
+                        <i className="fa-solid fa-hashtag hashtag-icon" />
+                        <input
+                            value={name}
+                            placeholder='new-channel'
+                            onChange={e => setName(e.target.value.replace(' ', '-').toLowerCase())}
+                        />
+                    </div>
+                    <div className='create-channel-error-container'>
+                        {errors.map(error => (
+                            <div>{error}</div>
+                        ))}
+                    </div>
+                </div>
+                <div className='create-channel-bottom-container'>
+                    <div className='create-channel-cancel'>
+                        Cancel
+                    </div>
+                    <div onClick={() => submitHandler()} className='create-channel-submit-button'>
+                        Create Channel
+                    </div>
+                </div>
             </div>
         </form>
     );
