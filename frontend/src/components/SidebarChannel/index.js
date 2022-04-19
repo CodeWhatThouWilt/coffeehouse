e872c50b658616f13c890fe9ae3a3da14b770647
Jacob North <yakenorth@protonmail.com>
2022-04-18 21:29:04 -0400

    Fixed error that was appearing when clicking on channel

--- frontend/src/components/SidebarChannel/index.js
+++ frontend/src/components/SidebarChannel/index.js
@@ -1,23 +1,32 @@
 import './SidebarChannel.css';
 import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { Modal } from '../../context/modal';
 import { useState } from 'react';
 import ChannelSettings from '../ChannelSettings';
+import { getChannelMessages } from '../../store/servers';
+import { useDispatch } from 'react-redux';
 
 const SidebarChannel = ({ channel }) => {
     const { serverId } = useParams();
+    const dispatch = useDispatch();
+    const history = useHistory();
     const [showModal, setShowModal] = useState(false);
+
+    const clickHandler = () => {
+        dispatch(getChannelMessages({ serverId, channelId: channel.id}))
+            .then(() => history.push(`/channels/${serverId}/${channel.id}`))
+    };
 
     return (
         <>
             <nav className='channel-container'>
-                <Link to={`/channels/${serverId}/${channel.id}`} className='channel-link'>
+                <div onClick={clickHandler} className='channel-link'>
                     <div className='channel-name-container'>
                         <i className="fa-solid fa-hashtag edit-channel-icon"></i>
                         <div>{channel.name}</div>
                     </div>
-                </Link>
+                </div>
                 <div onClick={() => setShowModal(true)}>
                     <i className="fa-solid fa-gear"></i>
                 </div>
