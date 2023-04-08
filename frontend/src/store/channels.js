08bf6d436f268d332aebbfad28a1cf96dc160137
Jacob North <yakenorth@protonmail.com>
2023-04-08 02:47:20 -0400

    Restructured channel reducer for deep copy / added nested state for byId and allIds

--- frontend/src/store/channels.js
+++ frontend/src/store/channels.js
@@ -1,27 +1,35 @@
 import { csrfFetch } from "./csrf";
 
 const ADD_CHANNEL = "channels/addChannel";
+const ADD_SERVER_CHANNELS = "channels/addServerChannels";
 const UPDATE_CHANNEL = "channels/updateChannel";
 const REMOVE_CHANNEL = "channels/removeChannel";
 
 const addChannel = (channel) => {
 	return {
 		type: ADD_CHANNEL,
-		channel,
+		payload: { channel },
+	};
+};
+
+const addServerChannels = (channels) => {
+	return {
+		type: ADD_SERVER_CHANNELS,
+		payload: { channels },
 	};
 };
 
 const updateChannel = (channel) => {
 	return {
 		type: UPDATE_CHANNEL,
-		channel,
+		payload: { channel },
 	};
 };
 
-const removeChannel = (idData) => {
+const removeChannel = (channelId) => {
 	return {
 		type: REMOVE_CHANNEL,
-		idData,
+		payload: { channelId },
 	};
 };
 
@@ -37,6 +45,15 @@ export const createChannel = (form) => async (dispatch) => {
 		return channel;
 	}
 	return res;
+};
+
+export const getServerChannels = (serverId) => async (dispatch) => {
+	const res = await csrfFetch(`/api/servers/${serverId}/channels`);
+
+	if (res.ok) {
+		const channels = await res.json();
+		dispatch(addServerChannels(channels));
+	}
 };
 
 export const editChannel = (form) => async (disptach) => {
@@ -72,3 +89,65 @@ export const deleteChannel = (form) => async (dispatch) => {
 	}
 	return res;
 };
+
+const initialState = {
+	byId: {},
+	allIds: [],
+};
+
+const channelsReducer = (state = initialState, action) => {
+
+	switch (action.type) {
+		case ADD_CHANNEL:
+			const { channel } = action.payload;
+
+			return {
+				byId: { ...state.byId, [channel.id]: channel },
+				allIds: [...state.allIds, channel.id],
+			};
+
+		case ADD_SERVER_CHANNELS: {
+			const { channels } = action.payload;
+
+			const channelsById = channels.reduce((acc, channel) => {
+				acc[channel.id] = channel;
+				return acc;
+			}, {});
+
+			const channelsIds = channels.map((channel) => channel.id);
+
+			return {
+				byId: { ...state.byId, ...channelsById },
+				allIds: [...state.allIds, ...channelsIds],
+			};
+		}
+		case UPDATE_CHANNEL: {
+			const { channel } = action.payload;
+
+			return {
+				byId: { ...state.byId, [channel.id]: channel },
+				allIds: [...state.allIds],
+			};
+		}
+		case REMOVE_CHANNEL: {
+			const { channelId } = action.payload;
+			const remainingChannelsById = { ...state.byId };
+			delete remainingChannelsById[channelId];
+			// alternatively named destructure / spread
+			// const { [channelId]: removedChannel, ...remainingChannels } = state.byId
+			const remainingChannelsAllIds = state.allIds.filter(
+				(id) => id !== channelId
+			);
+
+			return {
+				byId: remainingChannelsById,
+				allIds: remainingChannelsAllIds,
+			};
+		}
+		default: {
+			return state;
+		}
+	}
+};
+
+export default channelsReducer;
