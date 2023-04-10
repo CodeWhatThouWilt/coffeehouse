7dfe8e242c65de0984539632f4282d837a1a22e5
Jacob North <yakenorth@protonmail.com>
2023-04-09 20:14:06 -0400

    added reducer / actions for messages state. Partial complete, still needs thunks

--- frontend/src/store/messages.js
+++ frontend/src/store/messages.js
@@ -3,4 +3,81 @@ const ADD_MESSAGE = "servers/addMessage";
 const EDIT_MESSAGE = "servers/editMessage";
 
 
+const getMessages = (messages) => {
+	return {
+		type: GET_MESSAGES,
+		payload: { messages }
+	};
+};
+
+export const addMessage = (message) => {
+	return {
+		type: ADD_MESSAGE,
+		payload: { message }
+	};
+};
+
+const editMessage = (message) => {
+	return {
+		type: EDIT_MESSAGE,
+		payload: { message }
+	};
+};
+
 // TODO finish messages state to match channels with the exception of: add a byChannelId to initial state for easy tracking / filtering
+
+const initialState = {
+	byId: {},
+    byChannelId: {},
+	allIds: [],
+};
+
+const messagesReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+
+        case GET_MESSAGES: {
+            const { messages } = action.payload;
+
+            const messagesById = messages.reduce((acc, message) => {
+                acc[message.id] = message;
+                return acc; 
+            }, {});
+
+            const messagesByChannelId = messages.reduce((acc, message) => {
+                acc[message.channelId] = message;
+                return acc;
+            }, {});
+
+            const messagesIds = messages.map((message) => message.id);
+
+            return {
+                byId: { ...state.byId, ...messagesById },
+                byChannelId: { ...state.byChannelId, ...messagesByChannelId },
+                allIds: [...state.allIds, ...messagesIds]
+            };
+        }
+        case ADD_MESSAGE: {
+            const { message } = action.payload;
+
+            return {
+                byId: { ...state.byId, [message.id]: message },
+                byChannelId: { ...state.byId, [message.channelId]: message },
+                allIds: [...state.allIds, message.id]
+            };
+        }
+        case EDIT_MESSAGE: {
+            const { message } = action.payload;
+
+            return {
+				byId: { ...state.byId, [message.id]: message },
+				byChannelId: { ...state.byId, [message.channelId]: message },
+				allIds: [...state.allIds],
+			};
+        }
+
+        default: {
+            return state
+        }
+    }
+}
